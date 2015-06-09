a1e6f30d5e18c18780132970e273671988785545
FKrauss <fabiok@stabel.com>
2015-06-09 13:50:35 +0200

    Create WeekDayMobileBids.js
    This one changes the Mobile Bid Adjustment according to the current day. Set it to run Daily

---
+++ WeekDayMobileBids.js
@@ -0,0 +1,45 @@
+/*
+author: fabio@momondo.com
+
+Inputs for mobile bids based on weekday
+*/
+
+
+function main() {
+var campaignfilter = "DEST";  // add the terms the account must have to get the adjustments
+var campaignnegfilter = "App"; // add the term the account must not have to get the adjustments
+var selector = AdWordsApp.campaigns()
+              .withCondition("CampaignName CONTAINS \'"+ campaignfilter+"\'")
+              .withCondition("CampaignName DOES_NOT_CONTAIN_IGNORE_CASE \'"+ campaignnegfilter+"\'");
+
+switch (new Date().getDay()) {
+    case 0:
+        bid = 0.1; //Sunday
+        break;
+    case 1:
+        bid = -0.3; // Monday
+        break;
+    case 2:
+        bid = 0.2; // Tuesday
+        break;
+    case 3:
+        bid = -0.3; //Wednesday
+        break;
+    case 4:
+        bid = -0.3; //Thursday
+        break;
+    case 5:
+        bid = -0.3; // Friday
+        break;
+    case 6:
+        bid = 0.1; //Saturday
+    break;  }
+
+var campaignIterator = selector.get();
+  while(campaignIterator.hasNext()) {
+    var campaign = campaignIterator.next();
+    var MobileBidAdjustment = campaign.targeting().platforms().mobile().get().next();
+      MobileBidAdjustment.setBidModifier(1+bid);
+      Logger.log(campaign.getName()+" modified to "+ (1+bid)*100 +"%");
+  }
+}
